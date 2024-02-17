338adbeadb4772a7b067bf17bbac85ab4a00e8d4
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-02-17 04:55:03 -0500

    frame starter

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,29 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// ./app/page.tsx
+ 
+import { FrameContainer, FrameImage, FrameButton, getFrameMessage, useFramesReducer, getPreviousFrame, validateActionSignature, FrameInput } from "frames.js/next/server";
 
-export default function Home() {
+//@ts-ignore
+const reducer = (state, action) => ({ count: state.count + 1 });
+ 
+//@ts-ignore
+export default async function Home(props) {
+  const previousFrame = getPreviousFrame(props.searchParams);
+  const frameMessage = await getFrameMessage(previousFrame.postBody);
+ 
+  if (frameMessage && !frameMessage?.isValid) {
+    throw new Error("Invalid frame payload");
+  }
+  const [state, dispatch] = useFramesReducer(reducer, { count: 0 }, previousFrame);
+ 
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <FrameContainer postUrl="/frames" state={state} previousFrame={previousFrame}>
+      <FrameImage>
+        <div tw="w-full h-full bg-slate-700 text-white justify-center items-center">
+          {state.count}
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </FrameImage>
+      {/* @ts-ignore */}
+      <FrameButton onClick={dispatch}>{state.count}</FrameButton>
+    </FrameContainer>
   );
 }
